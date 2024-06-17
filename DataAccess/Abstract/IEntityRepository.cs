cd5c55e6c5c0aa159219eeaacf086031205b28cc
Emre Arslan <arslann1907@hotmail.com>
2024-06-18 00:00:29 +0200

    Add project files.

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //Generic constraint (generik kisitlama) where T:class welke T? enkel class
+    //Class : referans tip olacak, IEntitiyden implement edecek yada IEntitye bagli class
+    //IEntity : Ientity olabilir, veya implemente eden bir nesne (concrete maptaki)
+    //new() : new'lenebilir olmali verdigin <-->
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //Context file : Db tablolari ile proje classlarini bagliyoruz. (Db Customer verbinden met project Customer class)
+    public class NorthwindContext : DbContext
+    {
+        //Db connection string ekledik buraya
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Northwind;Trusted_connection=true");
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
