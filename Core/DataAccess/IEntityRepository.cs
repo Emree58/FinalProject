363226910193d7b9c02d346348c1214c9feb78b1
Emre Arslan <arslann1907@hotmail.com>
2024-06-25 20:26:40 +0200

    Ders 9, mola1 sonrasi

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,10 +1,10 @@
-﻿using Entities.Abstract;
+﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess
 {
     //Generic constraint (generik kisitlama) where T:class welke T? enkel class
     //Class : referans tip olacak, IEntitiyden implement edecek yada IEntitye bagli class
